5d9793ce45d0052ed69e8c98441171aeac03ed3f
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2025-10-10 19:15:05 +0530

    Ok

--- src/app.py
+++ src/app.py
@@ -21,7 +21,7 @@ st.markdown("""
 BASE_URL = "https://api.polygon.io/v2/aggs/ticker"
 API_KEY = os.environ.get("POLYGON_API_KEY1")
 START_DATE = "2015-01-01"
-END_DATE = "2025-08-31"
+END_DATE = "2025-10-31"
 
 # --- Company Options ---
 COMPANIES = {
